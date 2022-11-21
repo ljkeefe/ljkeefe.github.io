154a41e5e07eb53777013594a0b7e9eb7aac8fe0
Liam Keefe <77698938+ljkeefe@users.noreply.github.com>
2022-11-21 02:19:24 -0800

    Update page_2.py

--- page_2.py
+++ page_2.py
@@ -8,6 +8,7 @@ from dash import html, dcc
 from dash import dash_table
 import pandas as pd
 from navbar import create_navbar
+from app import app
 
 nav = create_navbar()
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
