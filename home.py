59c8834538821f6824be6f538818444e69e6fbe4
Liam Keefe <77698938+ljkeefe@users.noreply.github.com>
2022-11-21 05:10:07 -0800

    Update home.py

--- home.py
+++ home.py
@@ -12,6 +12,11 @@ nav = create_navbar()
 def create_page_home():
     header = html.Div(children=[
         html.H1('Welcome to the Analysis Dashboard!', style={'color' : '#0480B4'}),
+        html.Br(),
+        html.H3('Project List'),
+        html.A('Project 1 - MaxDiff Analysis', href='\page-2'),
+        html.Br(),
+        html.A('Project 2 - TURF Analysis', href='\page-3')
         
         
     ], style={'margin-top': '15px', 'textAlign': 'center', 'color': '#0480B4'})
@@ -20,16 +25,3 @@ def create_page_home():
         header,
     ])
     return layout
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
