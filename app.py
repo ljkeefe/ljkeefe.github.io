885dd08d5305ec66d92d32ba3a951273a1dcb9d9
Liam Keefe <77698938+ljkeefe@users.noreply.github.com>
2022-11-21 05:56:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,3 @@
 from jupyter_dash import JupyterDash
 import dash_bootstrap_components as dbc
 app = JupyterDash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
-
-
-# In[ ]:
-
-
-
-
