3102b6522f4804244760c1a766da5b0a1056a466
Liam Keefe <77698938+ljkeefe@users.noreply.github.com>
2023-01-23 13:27:57 -0800

    Update data.py

--- data.py
+++ data.py
@@ -10,3 +10,25 @@ for i in [j for j in pie_df.columns if j != 'person']:
         pie_df[i] = listy
     else:
         pie_df[i] = listy
+
+        
+MD_agg_df = pd.DataFrame(columns = ['Item', 'Item #', 'MaxDiff Score'])
+
+MD_agg_df['Item'] = item_list
+MD_agg_df['Item #'] = list(range(1,len(item_list)+1))
+
+perc = 1
+for i in item_list:
+    j = np.random.uniform(0,perc)
+    MD_agg_df.loc[MD_agg_df[''] == i, 'MaxDiff Score'] = j
+    perc -= j
+MD_agg_df.sort_values('MaxDiff Score', ascending=False)
+
+MD_resp_df = pd.DataFrame(columns = item_list)
+
+for i in list(range(500)):
+    perc = 1
+    for j in item_list:
+        k = np.random.uniform(0,perc)
+        MD_resp_df.loc[i, j] = k
+        perc -= k
